f7a62be1a9384ba5b53a2fa326503224c30b479f
tarripewe <tarripewe01@gmail.com>
2022-11-24 22:33:45 +0700

    Initial

---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+
+function Header() {
+  return (
+    <div>
+      <Navbar expand="lg" style={{ backgroundColor: "#ffbf69" }}>
+        <Container>
+          <Navbar.Brand
+            href="#home"
+            style={{ fontWeight: "bold", color: "#333333", fontSize: 25 }}
+          >
+            StartPlay
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse
+            id="basic-navbar-nav"
+            className="justify-content-end"
+          >
+            <Nav>
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Product</Nav.Link>
+              <Nav.Link href="#contact">Contact</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+}
+
+export default Header;
