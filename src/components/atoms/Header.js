c14210340695c2aac8ec06bcdca6dddbc4176ee7
tarripewe <tarripewe01@gmail.com>
2022-11-25 17:27:07 +0700

    Responsive Header

--- src/components/atoms/Header.js
+++ src/components/atoms/Header.js
@@ -1,31 +1,25 @@
 import React from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
 
+import "./Header.css";
+
 function Header() {
   return (
-    <div>
-      <Navbar expand="md" style={{ backgroundColor: "#ffbf69" }}>
-        <Container>
-          <Navbar.Brand
-            href="#home"
-            style={{ fontWeight: "bold", color: "#333333", fontSize: 25 }}
-          >
-            StartPlay
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse
-            id="basic-navbar-nav"
-            className="justify-content-end"
-          >
-            <Nav>
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Product</Nav.Link>
-              <Nav.Link href="#contact">Contact</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </div>
+    <Navbar expand="md" style={{ backgroundColor: "#ffbf69" }}>
+      <Container className="container">
+        <Navbar.Brand href="#home" className="navbarBrand">
+          StartPlay
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+          <Nav className="navLink">
+            <Nav.Link className="navLink-link" href="#home">Home</Nav.Link>
+            <Nav.Link className="navLink-link" href="#link">Product</Nav.Link>
+            <Nav.Link className="navLink-link" href="#contact">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
